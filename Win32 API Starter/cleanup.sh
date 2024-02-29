85cb6b2c1a05951eac885254e520edc891fe8983
mattbarich <mattbarich1@gmail.com>
2024-02-28 20:33:17 -0700

    Simple Process Creation & Simple Cleanup script

---
+++ Win32 API Starter/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#clean up directory
+rm *.exe
---
+++ Win32 API Starter/simple_process.c
@@ -0,0 +1,28 @@
+#include <windows.h>
+#include <stdio.h>
+#include <winbase.h>
+
+int main(void){
+
+STARTUPINFOW SI = { 0 };
+PROCESS_INFORMATION PI = { 0 };
+
+    if (!CreateProcessW(
+        L"C:\\Windows\\System32\\calc.exe",
+        NULL,
+        NULL,
+        NULL,
+        FALSE,
+        BELOW_NORMAL_PRIORITY_CLASS,
+        NULL,
+        NULL,
+        &SI,
+        &PI
+    ))     {
+        printf("(-) Process did not start :(. Error: %ld", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    printf("(+) Process started! PID: %ld\n", PI.dwProcessId);
+    return EXIT_SUCCESS;
+}
